d16ec5eca762f0eea61c0be257860359c5a2f8b4
Maxrozvora <rozvora.max@gmail.com>
2019-08-01 21:17:24 +0300

    add slider component

--- crud-hot-dog/src/App.vue
+++ crud-hot-dog/src/App.vue
@@ -2,32 +2,24 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>CRUD</span>
+        <span class="font-weight-light">HOT DOG</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Slider></Slider>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Slider from './components/Slider';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Slider
   },
   data: () => ({
     //
