48219444ac5b4eea42f348076cfe36e652927ec6
Denis Hyrin <d.hyrin@clever-sales.com>
2019-08-02 09:57:22 +0300

    adding and showing checkboxes and data

--- crud-hot-dog/src/components/Form.vue
+++ crud-hot-dog/src/components/Form.vue
@@ -9,14 +9,25 @@
 				<v-card-text>
 					<span>Base:</span>
 					<v-radio-group v-model="base" row>
-						<v-radio v-for="(type, i) in baseType" :key="i" :label="type" :value="type"></v-radio>
+						<v-radio v-for="(type, i) in baseType" :key="i" :label="type.name" :value="type"></v-radio>
 					</v-radio-group>
+					<span>Sausages:</span>
+					<v-radio-group v-model="sausages" row>
+						<v-radio v-for="(type, i) in sausagesType" :key="i" :label="type.name" :value="type"></v-radio>
+					</v-radio-group>
+					<span>Additional ingredients:</span>
+					<v-flex row>
+						<v-checkbox v-for="(item, i) in additionalIngredients" :key="i" v-model="item.added" :label="item.name" class="mx-2"></v-checkbox>
+					</v-flex>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
 					<v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
 				</v-card-actions>
+				<pre>
+					{{$data}}
+				</pre>
 			</v-card>
 		</v-dialog>
 	</v-layout>
@@ -28,8 +39,51 @@
 		data: () => ({
 			dialog: false,
 			base: '',
+			sausages: '',
 			baseType: [
-			  'Bun', 'Pita'
+				{
+					name: 'Bun',
+					Price: 20
+				},
+				{
+					name: 'Pita',
+					price: 30
+				}
+			],
+			sausagesType: [
+				{name: 'Milk', price: 20}, {name: 'Hinting', price: 30}
+			],
+			additionalIngredients: [
+				{
+					name: 'Cheese',
+					price: 5,
+					added: false
+				},
+				{
+					name: 'Fried potatoes',
+					price: 5,
+					added: false
+				},
+				{
+					name: 'Carrots',
+					price: 3,
+					added: false
+				},
+				{
+					name: 'Cabbage',
+					price: 3,
+					added: false
+				},
+				{
+					name: 'Tomatoes',
+					price: 5,
+					added: false
+				},
+				{
+					name: 'Pickles',
+					price: 5,
+					added: false
+				}
 			]
 		})
   }
