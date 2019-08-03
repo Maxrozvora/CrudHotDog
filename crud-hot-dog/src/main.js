22f4a0768e0e25f9443eaecb74c7afa952c3d92a
Maxrozvora <rozvora.max@gmail.com>
2019-08-04 00:51:02 +0300

    add firebase

--- crud-hot-dog/src/main.js
+++ crud-hot-dog/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
+import * as fb from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAgS1wJBvIYgNvf8V3jX9TH4ABWHvObiDk",
+      authDomain: "hotdog-10c9d.firebaseapp.com",
+      databaseURL: "https://hotdog-10c9d.firebaseio.com",
+      projectId: "hotdog-10c9d",
+      storageBucket: "hotdog-10c9d.appspot.com",
+      messagingSenderId: "206340798640",
+      appId: "1:206340798640:web:bd7352b7d80228ec"
+    }
+    fb.initializeApp(firebaseConfig)
+  }
 }).$mount('#app')
